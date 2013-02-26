9a6cb813ffcd9ee454d25c05f4c910ba26965efe
Karlit0 <antonio.stipanovic@fer.hr>
2013-02-26 15:15:56 +0100

    1st

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omega::Application.config.secret_token = '3ba23eba349916704f45ea4d93bc00d2f3b297284e723265b027b4315068f7e29685e86d2a742ac2db9055b9e4cf78e8fb47f0b99316594f165ccd5a8ff665e7'
